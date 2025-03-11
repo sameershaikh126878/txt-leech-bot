e4b2203d2bbc6d2b55ced4413f2ddfd07eba809d
AnkitShakya95 <ankitpatni95@gmail.com>
2025-03-12 05:19:18 +0530

    Update main.py

--- modules/main.py
+++ modules/main.py
@@ -208,8 +208,8 @@ async def account_login(bot: Client, m: Message):
                     'webengage-luid': '00000187-6fe4-5d41-a530-26186858be4c',
                     'accept-encoding': 'gzip'
                 }
-                res = requests.get("https://api.classplusapp.com/cams/uploader/video/jw-signed-url", params=params, headers=headers).json()
-                print(res)
+               res = requests.get("https://api.classplusapp.com/cams/uploader/video/jw-signed-url", params=params, headers=headers).json()
+               print(res)
                 
                
             elif '/master.mpd' in url:
